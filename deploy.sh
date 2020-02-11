909a7c1ee4c4a958a6813bc741b03741a77a4bda
Rui Vieira <ruidevieira@googlemail.com>
2020-02-11 13:24:48 +0000

    Remove Camel

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:defau
 
 oc apply -f deployment/ccfd-kafka.yaml
 
-oc expose svc/ccfd-kafka-brokers
-
 oc wait kafka/ccfd --for=condition=Ready --timeout=300s
+
+oc expose svc/ccfd-kafka-brokers
 
 oc new-app python:3.6~https://github.com/ruivieira/ccfd-kafka-producer \
 	-e BROKER_URL=ccfd-kafka-brokers:9092 \
@@ -19,10 +19,19 @@ oc new-app ruivieira/jbpm-seldon-test-model
 
 oc expose svc/jbpm-seldon-test-model
 
+# build the service
+sh build.sh
+
+# build the camel router
+mvn -f ../ccfd-fuse/pom.xml -P openshift
+
 # after running build.sh
 oc new-app ccd-service:1.0-SNAPSHOT \
-    -e BROKER_URL=ccfd-kafka-brokers:9092 \
-    -e SELDON_URL=http://jbpm-seldon-test-model-default.apps-crc.testing \
-    -e KAFKA_TOPIC=ccd
+    -e SELDON_URL=http://jbpm-seldon-test-model-default.apps-crc.testing
 
-oc expose svc/ccfd-service
+oc expose svc/ccd-service
+
+oc new-app ccd-fuse:1.0-SNAPSHOT \
+    -e BROKER_URL=ccfd-kafka-brokers:9092 \
+    -e KAFKA_TOPIC=ccd \
+    -e KIE_SERVER_URL=ccd-service:8090
