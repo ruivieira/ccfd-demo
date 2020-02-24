51ad40b51bcf13e33045e30cc988cc239f62e330
Rui Vieira <ruidevieira@googlemail.com>
2020-02-24 22:07:45 +0000

    Revert to bundled KJARs

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 mvn -f ccd-model/pom.xml clean install
-mvn -f ccd-kjar/pom.xml clean install -P openshift
+mvn -f ccd-fraud-kjar/pom.xml clean install -P openshift
+mvn -f ccd-standard-kjar/pom.xml clean install -P openshift
 mvn -f ccd-service/pom.xml clean install -P openshift,h2
